f905530b611d36afdc26476e2ef28b208bc37c45
Prince Frimpong <113698558+loompesky@users.noreply.github.com>
2023-10-13 18:43:05 +0000

    from source code

--- main.tf
+++ main.tf
@@ -14,24 +14,15 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_route" "private_nat_gateway" {
-  count                = length(aws_route_table.private) > 0 ? 1 : 0
-  route_table_id       = element(aws_route_table.private[*].id, count.index)
-  # ... other configurations ...
-}
-
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  
 
   tags = {
     Terraform = "true"
@@ -40,9 +31,9 @@ module "blog_vpc" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -51,16 +42,13 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_sg" {
-  source = "terraform-aws-modules/security-group/aws"
+  source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-  name = "blog"
 
-  vpc_id = module.blog_vpc.public_subnets[0]
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
