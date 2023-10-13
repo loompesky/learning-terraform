187e03d9f7753c1bef3642766a839d9044e385f4
Prince Frimpong <113698558+loompesky@users.noreply.github.com>
2023-10-13 13:51:31 +0000

    create dev vpc

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
